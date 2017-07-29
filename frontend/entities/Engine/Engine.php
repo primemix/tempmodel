156e59e0375c2ab1a59cf088ba6b14717bad1bcd
primemix <monstrpro@gmail.com>
2017-07-30 01:15:42 +0300

    merge

--- frontend/entities/Engine/Engine.php
+++ frontend/entities/Engine/Engine.php
@@ -3,10 +3,6 @@
 namespace frontend\entities\Engine;
 
 use frontend\entities\Interfaces\InterfaceSearchProvider;
-<<<<<<< HEAD
-use Prophecy\Exception\Doubler\InterfaceNotFoundException;
-=======
->>>>>>> origin/dev
 
 /**
  * Class Engine
@@ -15,25 +11,14 @@ use Prophecy\Exception\Doubler\InterfaceNotFoundException;
 class Engine
 {
     /**
-<<<<<<< HEAD
      * @var InterfaceSearchProvider instanceoff
-=======
-     * @var InterfaceSearchProvider
->>>>>>> origin/dev
      */
     protected $requestProvider;
 
     /**
-     * Engine constructor.
-<<<<<<< HEAD
-     * @param $requestProvider
-     */
-    public function __construct($requestProvider)
-=======
      * @param InterfaceSearchProvider $requestProvider
      */
     public function __construct(InterfaceSearchProvider $requestProvider)
->>>>>>> origin/dev
     {
         $this->requestProvider = $requestProvider;
     }
@@ -47,7 +32,3 @@ class Engine
         return $request = $this->requestProvider->getRequest($textArea);
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/dev
